4f0a5b04bfca54877d08c4e8590a0c98d05c1637
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-10 14:28:30 +0330

    add test router

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const { router: testRouter } = require('./src/test');
+
 const app = express();
 
 app.set('port', process.env.PORT);
@@ -10,5 +12,7 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/test', testRouter);
 
 module.exports = app;
