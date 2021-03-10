9ca8cad32eb0373b1cd5bd876fbfed0f7242db5a
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-10 15:41:14 +0330

    add asyncWrapper & Boom

--- src/test/router.js
+++ src/test/router.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const Boom = require('@hapi/boom');
+
+const asyncWrapper = require('../../lib/asyncWrapper');
 
 router.get('/', (req, res) => res.gen(200, 'ok!', {}));
 
-router.get('/error', () => {
-    throw new Error('test error');
-});
+router.get(
+    '/error',
+    asyncWrapper(() => Promise.reject(Boom.badRequest('error test', { error: 'test!' })))
+);
 
 module.exports = router;
