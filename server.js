b62aaaf8d0f5436522bcf3f8aff860afb775a5f0
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-10 14:53:33 +0330

    add error handler

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const { router: testRouter } = require('./src/test');
+const errorHandlerMiddleware = require('./lib/errorHandler');
 
 const app = express();
 
 app.set('port', process.env.PORT);
 
+// middlewares
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// routes
 app.use('/test', testRouter);
+
+// error handlers
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
--- src/test/router.js
+++ src/test/router.js
@@ -2,4 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => res.gen(200, 'ok!', {}));
 
+router.get('/error', () => {
+    throw new Error('test error');
+});
+
 module.exports = router;
