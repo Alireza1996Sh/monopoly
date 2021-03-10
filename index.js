68eec9f70ea766192163add6209ee6ee59953a0a
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-10 15:47:50 +0330

    add eslint rule for arrow body style

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = require('./server');
 
 const start = async () => {
     await mongooseConnection();
-    app.listen(app.get('port'), () =>
+    app.listen(app.get('port'), () => {
         // eslint-disable-next-line no-console
-        console.log(`server is listening to port ${app.get('port')}.`)
-    );
+        console.log(`server is listening to port ${app.get('port')}.`);
+    });
 };
 
 start();
--- src/test/router.js
+++ src/test/router.js
@@ -3,11 +3,15 @@ const Boom = require('@hapi/boom');
 
 const asyncWrapper = require('../../lib/asyncWrapper');
 
-router.get('/', (req, res) => res.gen(200, 'ok!', {}));
+router.get('/', (req, res) => {
+    return res.gen(200, 'ok!', {});
+});
 
 router.get(
     '/error',
-    asyncWrapper(() => Promise.reject(Boom.badRequest('error test', { error: 'test!' })))
+    asyncWrapper(() => {
+        return Promise.reject(Boom.badRequest('error test', { error: 'test!' }));
+    })
 );
 
 module.exports = router;
