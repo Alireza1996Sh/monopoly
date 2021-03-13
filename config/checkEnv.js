0f8036649b89097640a005879f5aacdcfcf6ba1a
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-13 16:27:27 +0330

    add NODE_ENV evironment variable

--- config/checkEnv.js
+++ config/checkEnv.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const check = require('check-types');
 
-const { PORT, MONGODB_URI } = process.env;
+const { NODE_ENV, PORT, MONGODB_URI } = process.env;
+const validNodeEnvs = ['development', 'production'];
 
+check.assert.contains(validNodeEnvs, NODE_ENV);
 check.assert.integer(PORT && +PORT);
 check.assert.nonEmptyString(MONGODB_URI);
