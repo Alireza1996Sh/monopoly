2de4b72f7fc37c44400b0aca0cea314a7fb171de
Alireza1996 <alirezashalforoosh75@gmail.com>
2021-03-15 16:11:30 +0330

    add jest

--- config/checkEnv.js
+++ config/checkEnv.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const check = require('check-types');
 
 const { NODE_ENV, PORT, MONGODB_URI } = process.env;
-const validNodeEnvs = ['development', 'production'];
+const validNodeEnvs = ['development', 'production', 'test'];
 
-check.assert.contains(validNodeEnvs, NODE_ENV);
+check.assert.in(NODE_ENV, validNodeEnvs);
 check.assert.integer(PORT && +PORT);
 check.assert.nonEmptyString(MONGODB_URI);
---
+++ src/test/index.test.js
@@ -0,0 +1,23 @@
+require(`../../config/checkEnv`);
+const supertest = require('supertest');
+const app = require('../../server');
+
+const request = supertest(app);
+
+describe('Route: /test', () => {
+    it('It should response the GET method', async () => {
+        const res = await request.get('/test');
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.message).toEqual('ok!');
+    });
+});
+
+describe('Route: /test/error', () => {
+    it('It should throw the GET method', async () => {
+        const res = await request.get('/test/error');
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.message).toEqual('error test');
+    });
+});
